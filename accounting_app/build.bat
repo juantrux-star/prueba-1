819de0b7ba4f6f7f3e4dcd680e0377d14ff6909a
juantrux-star <juantrux@hotmail.es>
2025-12-31 16:49:22 +0100

    Delete accounting_app directory

--- accounting_app/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Compilando aplicacion contable...
-python -m PyInstaller --name "ContabilidadDespacho" --add-data "templates;templates" --add-data "static;static" --onefile --noconsole app.py
-echo.
-echo Compilacion terminada. El ejecutable esta en la carpeta 'dist'.
-pause
